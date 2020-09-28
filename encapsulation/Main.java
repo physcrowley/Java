139cfbf255ffd73839ba8d34fef62a33df4cd78f
David Crowley <CROWLDA@ecolecatholique.ca>
2020-09-28 00:37:38 -0400

    added documentation

--- encapsulation/Main.java
+++ encapsulation/Main.java
@@ -1,7 +1,15 @@
 package encapsulation;
-
+/**
+ * Simple encapsulation example
+ * @author David Crowley, crowlda@ecolecatholique.ca
+ */
 public class Main
 {
+    /**
+     * To create and use UnObjet instance, showing encapsulation
+     * of the name field
+     * @param args
+     */
     public static void main(String[] args) 
     {
         UnObjet o = new UnObjet();
--- encapsulation/UnObjet.java
+++ encapsulation/UnObjet.java
@@ -1,5 +1,10 @@
 package encapsulation;
 
+/**
+ * Example to demonstrate basic encapsulation using
+ * public getters and setters for a private field.
+ * @author David Crowley, crowlda@ecolecatholique.ca
+ */
 public class UnObjet 
 {
     // champ privé
@@ -7,11 +12,13 @@ public class UnObjet
 
 
     // méthodes publiques pour obtenir et modifier le champ
+    /** Public getter for the name field */
     public String getName()
     {
         return this.name;
     }
 
+    /** Public setter for the name field */
     public void setName(final String newName)
     {
         this.name = newName;
