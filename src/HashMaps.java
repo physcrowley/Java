68a6d9c923b4ecef4518c3defa1f105b318cd3bc
physcrowley <crowlda@ecolecatholique.ca>
2020-10-08 10:27:42 -0400

    add unfinishesd HashMaps.java example

--- src/HashMaps.java
+++ src/HashMaps.java
@@ -1,7 +1,42 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class HashMaps 
 {
     public static void main(final String[] args) 
     {
-        
+        Scanner in = new Scanner(System.in);
+        /* Les clés sont les noms, 
+        les valeurs sont les numéros de téléphone */
+        //HashMap<Type pour la clé, Type pour les valeurs>
+        //HashMap<K,V>
+        // les types doivent être des objet (Integer au lieu de int)
+        HashMap<String, Integer> favNumbers = new HashMap<String, Integer>();
+
+        String[] answers = new String[2];
+        String name = "";
+        int favNum = 0;
+        boolean getfavNumbers = true;
+        while(getfavNumbers)
+        {
+            System.out.println("Enter a name and favourite number, separated by a comma");
+            answers = in.nextLine().split(",");
+            name = answers[0];
+            favNum = Integer.parseInt(answers[1]);
+            if (favNumbers.containsKey(name)) System.out.println("Name already exists");
+            else 
+            {
+                favNumbers.put(name, favNum);
+                System.out.println("Entry added to database");
+            }
+            System.out.println("Enter a new set?");
+            
+        }
+        favNumbers.put("David", 9);
+        favNumbers.put("Joseph", 3);
+        favNumbers.put("Joseph", 6);
+
+        System.out.println(favNumbers.get("Joseph"));
+
     }
 }
